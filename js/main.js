a536688a16cd53c269bfb11d7e64150969001500
pjtunstall <pjtunstall@gmail.com>
2025-08-19 16:43:56 +0100

    Minor edit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import { Tiles } from "./Tiles.js";
 import { createEventHandlers } from "./eventHandlers.js";
 import { Milestones } from "./Milestones.js";
 
-console.log("7");
+console.log("8");
 
 const numberOfTiles = 3;
 const tiles = new Tiles(numberOfTiles);
