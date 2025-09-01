333d6385782c9d3334fbda474211dc65b8dc0118
pjtunstall <pjtunstall@gmail.com>
2025-09-01 08:11:46 +0100

    Refactor Milestones: remove superfluous fields and make private what can be private

--- js/classes/Milestones.js
+++ js/classes/Milestones.js
@@ -2,16 +2,15 @@ const viewPortHeight = window.innerHeight;
 
 export class Milestones {
   #container;
+  #milestones;
 
   constructor(numberOfTiles, container) {
     this.#container = container;
-    this.numberOfTiles = numberOfTiles;
-    this.totalHeight = viewPortHeight * numberOfTiles;
-    this.milestones = [];
+    this.#milestones = [];
     this.#createAll();
   }
 
-  add(tileIndex, offsetXPercent, offsetYPercent, label = "", color = "red") {
+  #add(tileIndex, offsetXPercent, offsetYPercent, label = "", color = "red") {
     const milestone = document.createElement("milestone-element");
     milestone.initialize(
       tileIndex,
@@ -22,34 +21,34 @@ export class Milestones {
       this.#container
     );
     this.#container.appendChild(milestone);
-    this.milestones.push(milestone);
+    this.#milestones.push(milestone);
     return milestone;
   }
 
   initializeXPositions(tiles) {
-    this.milestones.forEach((m) => m.setXPosition(tiles));
+    this.#milestones.forEach((m) => m.setXPosition(tiles));
   }
 
   updatePosition(tiles) {
-    this.milestones.forEach((m) => m.updatePosition(tiles));
+    this.#milestones.forEach((m) => m.updatePosition(tiles));
   }
 
   forEach(callback) {
-    this.milestones.forEach(callback);
+    this.#milestones.forEach(callback);
   }
 
   #createAll() {
-    this.add(1, -12, 0, "Task", "red");
-    this.add(1, 0, -20, "Microtask Checkpoint #1", "yellow");
-    this.add(1, 13, -33, "MutationObserver Callbacks", "green");
-    this.add(1, 5, -45, "Microtask Checkpoint #2", "yellow");
-    this.add(0, -16, 12, "requestAnimationFrame Callbacks", "cyan");
-    this.add(0, -22, -12, "Style", "orange");
-    this.add(0, 34, -24, "Layout", "purple");
-    this.add(0, 21, -49, "ResizeObserver Callbacks", "gold");
-    this.add(2, 18, 33, "Paint", "blue");
-    this.add(2, -29, -13, "Composite", "silver");
-    this.add(2, -25, -27, "IntersectionObserver Callbacks", "white");
-    this.add(2, -12, -64, "requestIdleCallback Callbacks", "black");
+    this.#add(1, -12, 0, "Task", "red");
+    this.#add(1, 0, -20, "Microtask Checkpoint #1", "yellow");
+    this.#add(1, 13, -33, "MutationObserver Callbacks", "green");
+    this.#add(1, 5, -45, "Microtask Checkpoint #2", "yellow");
+    this.#add(0, -16, 12, "requestAnimationFrame Callbacks", "cyan");
+    this.#add(0, -22, -12, "Style", "orange");
+    this.#add(0, 34, -24, "Layout", "purple");
+    this.#add(0, 21, -49, "ResizeObserver Callbacks", "gold");
+    this.#add(2, 18, 33, "Paint", "blue");
+    this.#add(2, -29, -13, "Composite", "silver");
+    this.#add(2, -25, -27, "IntersectionObserver Callbacks", "white");
+    this.#add(2, -12, -64, "requestIdleCallback Callbacks", "black");
   }
 }
