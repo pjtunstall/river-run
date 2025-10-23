7f005ccc318a91426d8a42d9a4bbcad54ebe4bdf
pjtunstall <pjtunstall@gmail.com>
2025-10-23 01:02:52 +0100

    Better description of event loop

--- js/classes/Milestones.js
+++ js/classes/Milestones.js
@@ -39,16 +39,14 @@ export class Milestones {
 
   #createAll() {
     this.#add(1, -12, 0, "Task", "red");
-    this.#add(1, 0, -20, "Microtask Checkpoint #1", "yellow");
-    this.#add(1, 13, -33, "MutationObserver Callbacks", "green");
-    this.#add(1, 5, -45, "Microtask Checkpoint #2", "yellow");
-    this.#add(0, -16, 12, "requestAnimationFrame Callbacks", "cyan");
-    this.#add(0, -22, -12, "Style", "orange");
-    this.#add(0, 34, -24, "Layout", "purple");
-    this.#add(0, 21, -49, "ResizeObserver Callbacks", "gold");
+    this.#add(1, 0, -20, "Microtask Checkpoint", "yellow");
+    this.#add(1, 5, -45, "requestAnimationFrame Callbacks", "cyan");
+    this.#add(0, -16, 12, "Style", "white");
+    this.#add(0, -22, -12, "Layout", "orange");
+    this.#add(0, 24, -24, "ResizeObserver Callbacks", "purple");
+    this.#add(0, 21, -49, "IntersectionObserver Callbacks", "gold");
     this.#add(2, 18, 33, "Paint", "blue");
     this.#add(2, -29, -13, "Composite", "silver");
-    this.#add(2, -25, -27, "IntersectionObserver Callbacks", "white");
     this.#add(2, -12, -64, "requestIdleCallback Callbacks", "black");
   }
 }
